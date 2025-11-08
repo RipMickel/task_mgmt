6f93e53d706722857a6332bc0ad611537c9eea07
RipMickel <gaudicosmickelangelo@gmail.com>
2025-11-08 19:35:11 +0800

    Added scripts

--- coordinator/dashboard.php
+++ coordinator/dashboard.php
@@ -55,6 +55,9 @@ $instructorProgress = $progressStmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <title>Coordinator Dashboard</title>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <style>
         body {
